27e1698152333b2597ce8a1e65a0f1fda2c88cb8
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-18 00:49:44 +0300

    added

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  
+  return tutorials.map(tutorial => {
+   const words = tutorial.split(' ');
+   const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+   return capitalizedWords.join(' ');
+ });
+};
+
+
+console.log(titleCased());
